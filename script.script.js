9c90aeda2fa913443d56069b332782c02f8ac911
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-09-19 18:14:58 -0500

    add refreshed JavaScript/HTML

---
+++ script.script.js
@@ -0,0 +1,79 @@
+﻿
+
+let justiceName = document.getElementById("justice-name");
+let justiceNameButton = document.getElementById("justice-name-button");
+let presidentSelect = document.getElementById("president-select");
+let resultsList = document.getElementById("results-list");
+
+
+
+async function loadRecords() {
+	let response = await fetch(
+		"https://student-data-api.jenna-a-cardenas.workers.dev/api/v1/datasets/Supreme-Court-Justice/records?limit=114search="
+		+ presidentSelect.value
+	);
+
+	 let data = await response.json();
+  console.log("Status" + response.status);
+
+  let records = data.records;
+  console.log("Records: " + records.length);
+  //console.log("firstRecord: ", data.records[0]["Nominated by"])
+
+
+  resultsList.innerHTML = ""
+
+ 
+  if(records.length == 0){
+    resultsList.innerHTML = `<h3>No Information Found</h3>`
+  }
+
+  records.forEach(record => {
+    resultsList.innerHTML += `
+    <div>
+    <p>Nominated By: ${record["Nominated by"]}</p>
+    <p>Position: ${record.Position}</p>
+    <img src="${record.Image}" alt="President"/>
+    </div>
+    `;
+  })
+
+}
+
+justiceNameButton.addEventListener("click", async function () {
+	let record = await loadRecords(presidentSelect.value);
+	
+	if (!record) return;
+	
+	resultsList.textContent = record.Name;
+	console.log("record: ", record.Name);
+});
+
+function buildTrends(records) {
+	let recordsArray = [];
+	
+
+
+
+	records.forEach(function (record) {
+		
+		if (record["Nominated by"] == presidentSelect.value) {
+			recordsArray.push(record);
+		}
+	});
+	console.log("recordsArray: ", recordsArray);
+}
+
+const url = "https://student-data-api.jenna-a-cardenas.workers.dev/api/v1/datasets/Supreme-Court-Justice/records?limit=114search="
+
+async function loadRecords() {
+	const response = await fetch(url);
+	const data = await response.json();
+	
+	console.log(data.records[0]
+);
+	
+data.records.forEach(president => {
+	console.log(president.Justice);
+});
+}

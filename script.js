e24fd7f3d8d06677052223c034ced966c794a114
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-09-21 14:50:49 -0500

    add commented out codes for testing

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let stateSearchButton = document.getElementById("state-search-button");
 let index = 0
 let recordsArray = [];
 
-
+console.log("President select:", presidentSelect.value)
 async function loadRecords() {
 	let response = await fetch("https://student-data-api.jenna-a-cardenas.workers.dev/api/v1/datasets/Supreme-Court-Justice/records?limit=114&search");
 	console.log("Status" + response.status);
@@ -21,15 +21,15 @@ async function loadRecords() {
 	showRecord();
 }
 
-function showRecord() {
+/*function showRecord() {
  let record = records[index];
  
  document.getElementById("").textContent = record[""];
  document.getElementById("").textContent = record.JusticeNumber + " - " + record.State;
  document.getElementById("").textcontent = (index + 1) + " of " + records.length	
-}
+}*/
 	
-function buildTrends() {
+/*function buildTrends() {
 	let record = recordsArray[index];
 }	
 	recordsArray.forEach(function (Nominatedby) {
@@ -41,13 +41,37 @@ function buildTrends() {
 	recordsArray.forEach(function (record) {
 		
 		buildTrends(records);
-});
+});*/
 
-recordsButton.addEventListener("click", function () {
-	let record = loadRecords(presidentSelect.value);
+/*function buildTrends(records) {
+	let recordsArray = [];
+	
+
+
+
+	records.forEach(function (record) {
+		
+		if (record["Nominated by"] == presidentSelect.value) {
+			recordsArray.push(record);
+		}
+	});
+	console.log("recordsArray: ", recordsArray);
+}*/
+
+/*recordsButton.addEventListener("click", async function () {
+	let record = await loadRecords(presidentSelect.value);
 	
 	if (!record) return;
 	
 	resultsList.textContent = record.Name;
 	console.log("record: ", record.Name);
-});
+});*/
+
+/*justiceNameButton.addEventListener("click", async function () {
+	let record = await loadRecords(presidentSelect.value);
+	
+	if (!record) return;
+	
+	resultsList.textContent = record.Name;
+	console.log("record: ", record.Name);
+});*/
